b293864ce10e6e968e4f3d1909413312e6bdb510
Asia Keomanivong <asia.keomanivong@gmail.com>
2021-03-18 04:46:30 -0600

    stuff

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -10,8 +10,14 @@ app.component('book-display',{
         /*html*/
         `<ul class="col-sm-12 col-md-6 col-xl-4">
         <a class="list-group-item list-group-item-action active" :href="this.bookObj.selfLink">{{this.bookObj.volumeInfo.title}}</a>
-        <li class="list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
-        <li class="list-group-item">{{this.bookObj.volumeInfo.publisher}},{{this.bookObj.volumeInfo.publishedDate}}</li>
+        <img :src="this.bookObj.volumeInfo.imageLinks.thumbnail" style="width:150px;height:200px;"/>
+        <li class="list-group-item" v-if="this.bookObj.volumeInfo.authors != undefined">Author(s): {{this.bookObj.volumeInfo.authors.toString()}} </li>
+        <!--li class="list-group-item" v-if="this.bookObj.volumeInfo.authors != undefined" v-for="author in this.bookObj.volumeInfo.authors">Author: {{author}}</li-->
+        <!--li class="list-group-item" v-if="this.bookObj.volumeInfo.authors != undefined">{{this.bookObj.volumeInfo.authors}}</li-->
+        <li class="list-group-item" v-if="this.bookObj.volumeInfo.publisher != undefined">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
+        <li class="list-group-item" v-if="this.bookObj.volumeInfo.pageCount != undefined">{{this.bookObj.volumeInfo.pageCount}} pages</li>
+        <li class="list-group-item" v-if="this.bookObj.volumeInfo.description != undefined">{{this.bookObj.volumeInfo.description}} pages</li>
+        <li class="list-group-item" v-if="this.bookObj.volumeInfo.averageRating != undefined">Average Rating: {{this.bookObj.volumeInfo.averageRating}}</li>
         </ul>`,
     computed:{
             bookObj(){
--- js/main.js
+++ js/main.js
@@ -3,14 +3,50 @@ const app = createApp({
     data() {
         return{
             keyword: '',
-            result: null
+            result: null,
+            currentIndex: 0,
+            notFirstPage: false,
+            begin: false
         }
     },
     methods: {
         searchGoogleBooks(){
+            this.begin = true;
+            var txtSearch = document.querySelector("#txtSearch");
+            if (txtSearch.value == "") {
+                document.querySelector("#mainDiv").classList.add("hasError");
+                document.querySelector("#mainDiv").innerHTML = "No search keyword provided..."
+                return;
+            }
+
+            document.querySelector("#mainDiv").classList.remove("hasError");
+
             fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=0&maxResults=20")
                 .then(response => response.json())
                 .then(json => this.result = json)
+        },
+        nextPage(){
+            this.currentIndex = this.currentIndex + 20;
+            this.notFirstPage = true;
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=" + this.currentIndex + "&maxResults=20")
+                .then(response => response.json())
+                .then(json => this.result = json)
+        },
+        previousPage(){
+            this.currentIndex = this.currentIndex - 20;
+            if(this.currentIndex <= 0){
+                this.notFirstPage = false;
+            }
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=" + this.currentIndex + "&maxResults=20")
+                .then(response => response.json())
+                .then(json => this.result = json)
+        },
+        reset() {
+            this.keyword = '';
+            this.result = null;
+            this.currentIndex = 0;
+            this.notFirstPage = false;
+            this.begin = false;
         }
     }
 });
